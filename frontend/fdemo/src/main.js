45ae6e27b763f8d56efac488d3d42392a1244c5f
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-09-26 15:54:54 +0300

    addedUserOperations

--- frontend/fdemo/src/main.js
+++ frontend/fdemo/src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import header from "@/components/Header";
 
 const app = createApp(App);
+app.component("Header",header);
 app.use(router);
 app.mount('#app');
--- frontend/fdemo/src/views/Add.vue
+++ frontend/fdemo/src/views/Add.vue
@@ -1,3 +1,58 @@
 <template>
-    <h1>Add </h1>
+<Header />
+<div class="container mt-3">
+  <form class="d-flex justify-content-center">
+      
+  <div class="form-row">
+      <h2>Add User</h2>
+    <div class="col pt-2">
+      <input type="text" class="form-control" placeholder="First name" v-model="user.name">
+    </div>
+    <div class="col pt-2">
+      <input type="text" class="form-control" placeholder="Last name" v-model="user.surname">
+    </div>
+    <div class="col pt-2">
+      <input type="number" class="form-control" placeholder="Age" v-model="user.age">
+    </div>
+  
+    <div class="col pt-2">
+      <input type="text" class="form-control" placeholder="Province" v-model="user.province">
+    </div>
+    <div class="col pt-2">
+      <input type="text" class="form-control" placeholder="District" v-model="user.district">
+    </div>
+      <button @click="addUser()" class="btn btn-primary mt-2">Ekle</button>
+  </div>
+</form>
+</div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+
+  data(){
+    return{
+       user : {
+         name:null,
+         surname:null,
+         age:null,
+         province:null,
+         district:null
+      },
+    }
+  },
+
+
+    methods:{
+        addUser() {
+         axios.post("http://localhost:8080/api/users/add",{...this.user}).then(add_response => {
+          console.log(add_response);
+          alert("işlem başarılı")
+          this.$router.push({name:"ListPage"})
+       });
+    }
+}
+}
+</script>
