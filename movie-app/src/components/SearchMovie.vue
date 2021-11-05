529df347b967bbb1401fd74ace97f6d58499fde1
Cumali Tezcan <34379535+cumalitezcan@users.noreply.github.com>
2021-11-05 18:22:01 +0300

    refreshConfig

--- movie-app/src/components/SearchMovie.vue
+++ movie-app/src/components/SearchMovie.vue
@@ -5,15 +5,10 @@
       <input type="search" v-model="searchValue" />
       <button @click="search()">Search</button>
 
-      <div>
-        Movie Number: {{searchTypeMovie}}
-      </div>
-      <div>
-        Fovorite Movie Number: {{favNumber}}
-      </div>
-
+      <div>Movie Number: {{ searchTypeMovie }}</div>
+      <div>Fovorite Movie Number: {{ favNumber }}</div>
     </div>
-      <b-spinner v-show="loading" variant="primary" />
+    <b-spinner v-show="loading" variant="primary" />
     <div v-if="searchResults !== []">
       <b-card
         class="m-3"
@@ -21,19 +16,22 @@
         :key="index"
         :title="movie.title"
         :sub-title="movie.titleType"
-        style="min-width: 20rem;"
+        style="min-width: 20rem"
       >
+        <b-button
+          v-if="movie.fav !== true"
+          @click="addFavorite(movie, 'add')"
+          variant="primary"
+        >
+          Add <b-badge variant="light">+</b-badge>
+        </b-button>
+        <b-button v-else @click="addFavorite(movie, 'del')" variant="danger">
+          Delete <b-badge variant="light">-</b-badge>
+        </b-button>
 
-      <b-button v-if="movie.fav !== true" @click="addFavorite(movie,'add')" variant="primary">
-        Add <b-badge variant="light">+</b-badge>
-      </b-button>
-      <b-button v-else @click="addFavorite(movie,'del')" variant="danger">
-        Delete <b-badge variant="light">-</b-badge>
-      </b-button>
-
-      <div v-if="movie.image">
-        <b-img :src="movie.image.url" fluid></b-img>
-      </div>
+        <div v-if="movie.image">
+          <b-img :src="movie.image.url" fluid></b-img>
+        </div>
         <b-card-text>
           Some quick example text to build on the <em>card title</em> and make
           up the bulk of the card's content.
@@ -41,7 +39,7 @@
 
         <b-card-text>A second paragraph of text in the card.</b-card-text>
 
-        <a href="#" class="card-link">{{movie.id}}</a>
+        <a href="#" class="card-link">{{ movie.id }}</a>
         <b-link href="#" class="card-link">Another link</b-link>
       </b-card>
     </div>
@@ -49,37 +47,51 @@
 </template>
 
 <script>
-import {mapGetters,mapState,mapActions,mapMutations} from 'vuex'
+import { mapGetters, mapState, mapActions, mapMutations } from "vuex";
 export default {
   name: "SearchMovie",
 
   data() {
     return {
       searchValue: "",
-      loading: false
+      loading: false,
     };
   },
 
   methods: {
-    ...mapActions(['searchFromMovieTitle']),
-    ...mapMutations(['SET_FAV']),
+    ...mapActions(["searchFromMovieTitle"]),
+    ...mapMutations(["SET_FAV"]),
     search() {
-       this.$router.push({name:'SearchMovie',query:{title:this.searchValue}})
-       this.loading = true
-      this.searchFromMovieTitle(this.searchValue).then(()=>{
-        this.loading = false
-      })
+      this.$router.push({
+        name: "SearchMovie",
+        query: { title: this.searchValue },
+      });
+      this.loading = true;
+      this.searchFromMovieTitle(this.searchValue).then(() => {
+        this.loading = false;
+      });
     },
-    addFavorite(movie,type){
-      this.SET_FAV({type,movie})
-    }
+    addFavorite(movie, type) {
+      this.SET_FAV({ type, movie });
+    },
   },
   computed: {
-    ...mapGetters(['searchTypeMovie','favNumber']),
-    ...mapState(['searchResults']),
+    ...mapGetters(["searchTypeMovie", "favNumber"]),
+    ...mapState(["searchResults"]),
     // searchResults() {
     //   return this.$store.state.searchResults;
     // },
+  },
+
+  //sayfa yenilenince arama listesinin gitmemesi
+  mounted() {
+    if (this.$route.query.title) {
+      this.loading = true;
+      this.searchValue = this.$route.query.title;
+      this.searchFromMovieTitle(this.searchValue).then(() => {
+        this.loading = false;
+      });
+    }
   },
 };
 </script>
